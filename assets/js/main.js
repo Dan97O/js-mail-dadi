c50bf67aa60f6d2a2cc3e0bd8f2469aa93e121ef
Dan97O <dan97-oddo@live.it>
2023-03-13 15:23:32 +0100

    add mail prompt

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,10 +9,56 @@ Che ci sia un array da qualche parte?
 Se dobbiamo confrontare qualcosa che "cosa" ci serve? */
 
 //chiedi all'utente la sua email
-
+const userMail = prompt('La tua email');
 //contralla che sia nella lista di chi puo' accedere
+const listMail = ['nome.cognome@email.com', 'dan97-oddo@live.it', 'prova'];
+
 
 //stampa un messaggio appropriato sull’esito del controllo.
+for (let i = 0; i < listMail.length; i++) {
+    if (listMail[i] === userMail) {
+      alert("Accesso Consentito");
+      console.log(listMail[i]);
+
+    } else (listMail[i] != userMail); {
+      alert("Accesso Negato!");
+      console.log(listMail[i]);
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //Bonus Mail
@@ -22,7 +68,35 @@ Se dobbiamo confrontare qualcosa che "cosa" ci serve? */
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Gioco dei dadi
 //Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 
 //Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
